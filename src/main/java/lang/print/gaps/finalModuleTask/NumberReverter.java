514a221f5ae2d159819894d15031e4efc6afb7b6
rtemirbulat <equalityisnotequality@gmail.com>
2024-01-29 19:16:53 +0600

    Finished!

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+       int hundreds = number/100;
+       int tens = number/10%10;
+       int ones = number%10;
+        System.out.println(ones*100+tens*10+hundreds);
     }
 }
